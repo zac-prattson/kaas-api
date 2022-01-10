43dc882ecb2aa6e2eb61c77fff33c7b5a99b0bbb
clyde-titanious <60722368+clyde-titanious@users.noreply.github.com>
2022-01-10 09:14:38 -0500

    fix: cors again

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 import datetime
 import requests
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def fetch_data():
   # --- Call KaaS REST endpoint ---
-  response.headers.add("Access-Control-Allow-Origin", "*")
   req = requests.get('https://api.kanye.rest/')
   resp = req.content
   return resp
